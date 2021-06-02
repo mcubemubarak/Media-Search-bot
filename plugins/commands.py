69e88beb7a75aae4a949f098bc4485e2740a3f9f
KL10Movies <65135315+muhssiin@users.noreply.github.com>
2021-06-03 00:03:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,7 +2,7 @@ import os
 import logging
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG
+from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG, HELP_MSG
 from utils import Media
 
 logger = logging.getLogger(__name__)
@@ -20,6 +20,19 @@ async def start(bot, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(START_MSG, reply_markup=reply_markup)
+
+@Client.on_message(filters.command('help'))
+async def start(bot, message):
+    """Help command handler"""
+    if len(message.command) > 1 and message.command[1] == 'subscribe':
+        await message.reply(INVITE_MSG)
+    else:
+        buttons = [[
+            InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('Go Inline', switch_inline_query=''),
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply(HELP_MSG, reply_markup=reply_markup)
 
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
